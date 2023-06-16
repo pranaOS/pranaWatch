8bbf1ac6d6dbbce938597f90f496705e86ad0e8e
krishpranav <krisna.pranav@gmail.com>
2023-06-16 18:31:12 +0530

    update LICENSE

---
+++ libraries/libutils/TimeUtils.h
@@ -0,0 +1,10 @@
+/**
+ * @file TimeUtils.h
+ * @author Krisna Pranav
+ * @brief Time Utils Header
+ * @version 0.1
+ * @date 2023-06-16
+ * 
+ * @copyright Copyright (c) 2023 Krisna Pranav, pranaOS Developers
+ * 
+ */
